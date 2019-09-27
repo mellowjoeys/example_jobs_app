7b8c199e2ab09e59d67ff3a2cc099fe21deab393
mellowjoeys <mellowjoey@gmail.com>
2019-09-27 11:07:17 -0500

    initial commit, figaro added

---
+++ app/controllers/api/jobs_controller.rb
@@ -0,0 +1,7 @@
+class Api::JobsController < ApplicationController
+  def show
+    response = HTTP.get("https://www.themuse.com/api/public/jobs?page=#{params["page"]}&api_key=#{ ENV['API_KEY']}")
+    @job_page = response.parse
+    render json: @job_page
+  end
+end
